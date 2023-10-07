79c3f665e3855a8274d6f68847868c9aaf8f577a
andrius <andriussvilys@gmail.com>
2023-10-07 17:57:02 +0300

    no proxy

---
+++ server/mongo.js
@@ -0,0 +1,31 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const dotenv = require('dotenv');
+dotenv.config();
+
+const uri = process.env.MONGODB_URI
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function connect() {
+    try {
+
+        await client.connect();
+
+        await client.db("admin").command({ ping: 1 });
+
+        console.log("**********************************")
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    }
+    catch(err){
+        console.log(err)
+    }
+}
+
+module.exports = {connect, client}
--- server/server.js
+++ server/server.js
@@ -1,10 +1,8 @@
 var express = require('express');
 var path = require('path');
 const cors = require('cors');
-const dotenv = require('dotenv');
-dotenv.config();
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+
 var app = express();
 app.use(cors());
 
@@ -13,20 +11,10 @@ var guestsRouter = require('./routes/guests');
 var menusRouter = require('./routes/menus');
 var dishesRouter = require('./routes/dishes');
 var bookingsRouter = require('./routes/bookings');
+const { connect } = require('./mongo');
 
 
-const uri = process.env.MONGODB_URI
 
-console.log("uri ***************************************************")
-console.log(uri)
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -43,19 +31,7 @@ app.listen(port, () => {
     console.log(`server runs on port ${port}`)
 })
 
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
-    }
-    catch(err){
-        console.log(err)
-    }
-}
-run()
+connect()
 
 
 module.exports = app;
