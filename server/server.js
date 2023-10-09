f77e73feb515a6e3e3dad0cce28f73af65d348ec
andrius <andriussvilys@gmail.com>
2023-10-09 14:00:11 +0300

    locally working version

--- server/server.js
+++ server/server.js
@@ -7,7 +7,6 @@ app.use(cors());
 
 var indexRouter = require('./routes/index');
 var bookingsRouter = require('./routes/bookings');
-const { connect } = require('./mongo');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -20,12 +19,5 @@ const port = '5000';
 app.listen(port, () => {
     console.log(`server runs on port ${port}`)
 })
-
-connect().then(res => {
-
-})
-
-
-
 
 module.exports = app;
