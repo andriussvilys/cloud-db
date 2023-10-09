e96bf660b8a05721c1f425fbabe26c67537962ce
andrius <andriussvilys@gmail.com>
2023-10-09 12:54:38 +0300

    port

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use( indexRouter);
 app.use( bookingsRouter)
 
-const port = '5000';
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log(`server runs on port ${port}`)
 })
